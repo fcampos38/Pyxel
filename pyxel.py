dce801b3d0dcbb6e987263294ee52cf0e4620a9f
Francisco Campos <francisco.campos38e@gmail.com>
2022-12-25 21:01:42 +0000

    Functionality increased on the Excel class
    Added a function to instantiate an WorksheetObject

--- pyxel.py
+++ pyxel.py
@@ -4,7 +4,6 @@
 Required Imports
 '''
 import win32com.client as win32
-import numpy as np
 from win32com.universal import com_error
 import logging
 import re
@@ -22,6 +21,11 @@ Creates a python class to represent a Microsoft Excel workbook
 A workbook object will result from the instantiation of this class
 '''
 class Excel():
+    # Defines required class variables
+    ExcelCol = ["".join(i) for i in itertools.product(string.ascii_uppercase, repeat=2)] + \
+               ["".join(i) for i in itertools.product(string.ascii_uppercase, repeat=2)] + \
+               ["".join(i) for i in itertools.product(string.ascii_uppercase, repeat=3)]
+    
     def __init__(self, PathToWorkbook: str, BackgroundExecution=True, OverWriteIfExists=False):
         """
         :param: PathToWorkbook: string representing the full path of the excel workbook
@@ -72,32 +76,69 @@ class Excel():
                 raise PyxelException(f"The workbook could not be opened: {error1}")
         
         # Gets all workbook's sheets into an array like attribute
-        self.worksheets = tuple((sheet for sheet in self.WorkbookObj.Sheets))
+        self.Worksheets = tuple((sheet for sheet in self.WorkbookObj.Sheets))
     
-    def save(self, SaveAs=None):
+    def Save(self, SaveAs=None):
         """
         :param: SaveAs: string representing the full path to use when saving he workbook, default is the WorkbookObj original path
 
         :return: None
         """
         
-        # When SaveAs is defined as a different path than the PathToWorkbook, the WorkbookObj will be referring to two workbooks at the same time
-        self.WorkbookObj.SaveAs(SaveAs if SaveAs is not None else self.PathToWorkbook)
+        try:
+            # When SaveAs is defined as a different path than the PathToWorkbook, the WorkbookObj will be referring to two workbooks at the same time
+            self.WorkbookObj.SaveAs(SaveAs if SaveAs is not None else self.PathToWorkbook)
+        except Exception as error:
+            raise PyxelException(f"Changes on the WorkbookObject could not be saved: {error}")
         
-    def close(self, Save=True):
+    def Close(self, Save=True):
         """
         :param: Save: boolean stating whether or not to save the workbook before closing it
 
         :return: None
         """
+        try:
+            # Closes the workbook object
+            self.WorkbookObj.Close(Save)
+            # Quits Microsoft Excel (terminates the dispatch command)
+            self.XlDispatch.Quit()
         
-        # Closes the workbook object
-        self.WorkbookObj.Close(Save)
-        # Quits Microsoft Excel (terminates the dispatch command)
-        self.XlDispatch.Quit()
+        except Exception as error:
+            raise PyxelException(f"WorkbookObject could not be closed: {error}")
+            
+        
+    def Worksheet(self, WorksheetName: str):
+        """
+        :param: WorksheetName: string representing the worksheet name
+        
+        :return: WorksheetObject
+        """
+        
+        # Tries to create the worksheet if it does not exist
+        if not WorksheetName in self.Worksheets:
+            try:
+                # Creates the worksheet 
+                worksheet = self.WorkbookObj.Sheets.Add(
+                        Before=None, 
+                        After=self.WorkbookObj.Sheets(len(self.Worksheets))
+                                                            )
+                worksheet.Name = WorksheetName
+
+                # Saves the changes
+                self.Save(SaveAs=None)
+                
+            except Exception as error:
+                raise PyxelException(f"Worksheet '{WorksheetName}' not found on the workbook and creation attempt failed: {error}")
+            
+            # Updates the Worksheets attribute
+            self.Worksheets = tuple((sheet for sheet in self.WorkbookObj.Sheets))
+            
+        # Initiates a WorksheetObject and returns it
+        WorksheetObj = self.WorkbookObj.Sheets(WorksheetName)
+        return WorksheetObj
                     
     def __del__(self):
-        self.close(Save=False)
+        self.Close(Save=False)
         
     def __repr__(self):
         return chr(39) + f'ExcelWorkbookObject for "{self.PathToWorkbook}"' + chr(39)
